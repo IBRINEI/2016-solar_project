789e78269e0ed782c87f7c36bfc06e277853199f
IBRINEI <55546925+IBRINEI@users.noreply.github.com>
2019-11-15 03:17:53 +0300

    Update solar_input.py

--- solar_input.py
+++ solar_input.py
@@ -20,7 +20,7 @@ def read_space_objects_data_from_file(input_filename):
                 continue  # пустые строки и строки-комментарии пропускаем
             object_type = line.split()[0].lower()
             print(object_type)
-            if object_type == "star":  # FIXME: do the same for planet
+            if object_type == "star":
                 star = Star()
                 print('2')
                 line = line.split()
@@ -29,7 +29,7 @@ def read_space_objects_data_from_file(input_filename):
                 print('3')
                 objects.append(star)
                 print('1')
-            elif object_type == "planet":  # FIXME: do the same for planet
+            elif object_type == "planet":
                 planet = Planet()
                 line = line.split()
                 print(line)
@@ -55,13 +55,13 @@ def parse_star_parameters(line, star):
     **star** — объект звезды.
     """
 
-    star.R = int(line[1])
+    star.R = float(line[1])
     star.color = line[2]
     star.m = float(line[3])
     star.x = float(line[4])
     star.y = float(line[5])
-    star.Vx = int(line[6])
-    star.Vy = int(line[7])
+    star.Vx = float(line[6])
+    star.Vy = float(line[7])
 
 
 def parse_planet_parameters(line, planet):
@@ -80,7 +80,7 @@ def parse_planet_parameters(line, planet):
     **planet** — объект планеты.
     """
 
-    planet.R = int(line[1])
+    planet.R = float(line[1])
     planet.color = line[2]
     planet.m = float(line[3])
     planet.x = float(line[4])
@@ -102,10 +102,8 @@ def write_space_objects_data_to_file(output_filename, space_objects):
     """
     with open(output_filename, 'w') as out_file:
         for obj in space_objects:
-            print(out_file, "%s %d %s %f %f %f %f" % (obj.type, obj.R, obj.m, obj.x, obj.y, obj.Vx, obj.Vy))
+            out_file.write("%s %f %s %f %f %f %f %f \n" % (obj.type, obj.R, obj.color, obj.m, obj.x, obj.y, obj.Vx, obj.Vy))
 
-
-# FIXME: хорошо бы ещё сделать функцию, сохранающую статистику в заданный файл...
 
 if __name__ == "__main__":
     print("This module is not for direct call!")
